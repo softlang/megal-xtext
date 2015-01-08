943e152bbdb6d1ca9fe28037167fab383278e350
johanneshaertel <johanneshaertel@uni-koblenz.de>
2015-01-08 19:22:08 +0100

    Some color.

--- org.softlang.megal.sirius/src/org/softlang/megal/sirius/MegalServices.java
+++ org.softlang.megal.sirius/src/org/softlang/megal/sirius/MegalServices.java
@@ -1,9 +1,11 @@
 package org.softlang.megal.sirius;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.softlang.megal.Annotation;
 import org.softlang.megal.Declaration;
 import org.softlang.megal.Entity;
 import org.softlang.megal.EntityType;
@@ -11,9 +13,17 @@ import org.softlang.megal.Megamodel;
 import org.softlang.megal.Relationship;
 import org.softlang.megal.RelationshipType;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.Definitions;
+
 public class MegalServices {
 
 	public final static MegalServices INSTANCE = new MegalServices();
+
+	private static final String COLOR = "Color";
+
+	private static final Integer COLOR_RED_DEFAULT = 255;
+	private static final Integer COLOR_GREEN_DEFAULT = 255;
+	private static final Integer COLOR_BLUE_DEFAULT = 255;
 
 	public Megamodel getMegamodel(Declaration declaration) {
 		return (Megamodel) declaration.eContainer();
@@ -92,6 +102,112 @@ public class MegalServices {
 		return megamodel.getVisibleDeclarations().stream().filter(x -> x instanceof Entity)
 				.map(x -> (Entity) x).filter(x -> name.equals(x.getName())).findFirst()
 				.orElse(null);
+	}
+
+	public Annotation getAnnotation(Declaration entity, String name) {
+		Optional<Annotation> color = entity.getInfo().stream().filter(x -> name.equals(x.getKey()))
+				.findAny();
+		return color.orElse(null);
+	}
+
+	private Integer get(Declaration declaration, int position) {
+
+		Annotation annotation = getAnnotation(declaration, COLOR);
+
+		if (annotation == null)
+			return null;
+
+		String[] colorStrings = annotation.getValue().split(",");
+
+		if (colorStrings.length != 3)
+			return null;
+
+		String colorString = colorStrings[position];
+
+		try {
+			Integer color = Integer.valueOf(colorString);
+
+			return color;
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
+	public Integer red(Entity entity) {
+
+		Integer color = get(entity, 0);
+
+		if (color == null)
+			color = get(entity.getType().getDefinition(), 0);
+
+		if (color == null)
+			color = COLOR_RED_DEFAULT;
+
+		return color;
+	}
+
+	public int green(Entity entity) {
+		Integer color = get(entity, 1);
+
+		if (color == null)
+			color = get(entity.getType().getDefinition(), 1);
+
+		if (color == null)
+			color = COLOR_GREEN_DEFAULT;
+
+		return color;
+	}
+
+	public int blue(Entity entity) {
+		Integer color = get(entity, 2);
+
+		if (color == null)
+			color = get(entity.getType().getDefinition(), 2);
+
+		if (color == null)
+			color = COLOR_BLUE_DEFAULT;
+
+		return color;
+	}
+
+	public Integer red(EntityType entityType) {
+
+		Integer color = get(entityType, 0);
+
+		if (color == null)
+			color = COLOR_RED_DEFAULT;
+
+		return color;
+	}
+
+	public int green(EntityType entityType) {
+		Integer color = get(entityType, 1);
+
+		if (color == null)
+			color = COLOR_GREEN_DEFAULT;
+
+		return color;
+	}
+
+	public int blue(EntityType entityType) {
+		Integer color = get(entityType, 2);
+
+		if (color == null)
+			color = COLOR_BLUE_DEFAULT;
+
+		return color;
+	}
+
+	public boolean canRemove(Entity entity) {
+		for (Declaration declaration : getMegamodel(entity).getDeclarations()) {
+			if (declaration instanceof Relationship) {
+				Relationship relationship = (Relationship) declaration;
+				if (relationship.getLeft().equals(entity) || relationship.getRight().equals(entity))
+					return false;
+			}
+		}
+
+		return true;
 	}
 
 }
