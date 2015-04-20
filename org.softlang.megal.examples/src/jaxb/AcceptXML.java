ef220a853422feb688191f0cc57acb688020ba64
Lukas Härtel <lukashaertel@googlemail.com>
2015-04-20 16:48:52 +0200

    Quick SAX implementation of the XML element of checker.

--- org.softlang.megal.examples/src/jaxb/AcceptXML.java
+++ org.softlang.megal.examples/src/jaxb/AcceptXML.java
@@ -1,16 +1,37 @@
 package jaxb;
 
-import org.softlang.megal.api.Output;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 
-import com.google.common.io.CharSource;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.softlang.megal.api.Output;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 import pluginroot.elementof.Acceptor;
 
 public class AcceptXML extends Acceptor {
 	@Override
 	public Output accept(String source) {
-		// TODO Auto-generated method stub
-		return Output.valid();
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setValidating(false);
+		factory.setNamespaceAware(true);
+
+		try (Reader r = new StringReader(source)) {
+			SAXParser parser = factory.newSAXParser();
+			XMLReader reader = parser.getXMLReader();
+
+			reader.parse(new InputSource(r));
+
+			return Output.valid();
+		} catch (IOException | SAXException | ParserConfigurationException e) {
+			return Output.error(e.getLocalizedMessage());
+		}
 	}
 
 }
--- org.softlang.megal.examples/src/pluginroot/elementof/Acceptor.java
+++ org.softlang.megal.examples/src/pluginroot/elementof/Acceptor.java
@@ -3,8 +3,6 @@ package pluginroot.elementof;
 import org.softlang.megal.api.Evaluator;
 import org.softlang.megal.api.Output;
 
-import com.google.common.io.CharSource;
-
 public abstract class Acceptor extends Evaluator {
 	public abstract Output accept(String text);
 }
