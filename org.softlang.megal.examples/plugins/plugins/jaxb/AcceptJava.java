b84074589d2111bbc7ffce3a911be271b7c8dcce
Lukas Härtel <lukashaertel@googlemail.com>
2015-05-15 17:04:46 +0200

    Added java parser

--- org.softlang.megal.examples/plugins/plugins/jaxb/AcceptJava.java
+++ org.softlang.megal.examples/plugins/plugins/jaxb/AcceptJava.java
@@ -1,19 +1,29 @@
 package plugins.jaxb;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.softlang.megal.mi2.api.Artifact;
 
 import plugins.root.elementof.Acceptor;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
 import com.google.common.base.Optional;
+import com.google.common.base.Throwables;
 
 public class AcceptJava extends Acceptor {
 	@Override
 	public Optional<String> accept(Artifact artifact) {
-		if (artifact.getLocation().getPath().toLowerCase().endsWith(".java"))
+		try (InputStream s = artifact.getBytes().openStream()) {
+			JavaParser.parse(s);
 			return Optional.absent();
-		else
+		} catch (ParseException e) {
 			return Optional.of("The artifact " + artifact.getName()
-					+ " is not an element of Java.");
+					+ " is not an element of Java, reason: " + e.getMessage());
+		} catch (IOException e) {
+			return Optional.of(Throwables.getStackTraceAsString(e));
+		}
 	}
 
 }
